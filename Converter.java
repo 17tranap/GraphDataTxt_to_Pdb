6f7c448f0723d97ce2e125a6fdc061a06135379e
alexandria <apt029@ucsd.edu>
2018-02-26 10:25:43 -0800

    cleaned up

--- Converter.java
+++ Converter.java
@@ -4,11 +4,31 @@
  * such that the data points are the first two columns of the .txt with the
  * first as the X coordinate and the second as a Y coordinate
  */
+import java.awt.*;
+import java.awt.Color;
+import javax.swing.*;
+import java.awt.event.*;
+import javafx.application.*;
 
+import javafx.scene.*;
+import javafx.scene.paint.*;
+import javafx.scene.shape.*;
+
+import javafx.stage.*;
+import java.util.*;
+import java.util.List;
 import java.io.*;
+import java.lang.Number;
+import java.lang.Math;
+import javax.swing.border.*;
+import javax.swing.filechooser.FileSystemView;
 
-public class Converter() {
+import java.lang.Object;
+
+
+public class Converter extends JFrame{
 	public static String txtName;
+    public static int numResidues;
 
 	JPanel[] row = new JPanel[15];
     int[] dimW = {300,45,100,90};
@@ -17,35 +37,20 @@ public class Converter() {
     Dimension regularDimension = new Dimension(dimW[0], dimH[1]);
     Dimension rColumnDimension = new Dimension(dimW[2], dimH[1]);
     Dimension zeroButDimension = new Dimension(dimW[3], dimH[1]);
-    //double[] temporary = {0, 0};
 
-    //JTextField file1 = new JTextField("",10);
-    //JTextField file2 = new JTextField("",10);
-    //JTextField refPoint = new JTextField("",10);
-    static JLabel message = new JLabel("");
-    static String option1 = new String("");
-    static String option2 = new String("");
-    static String option3 = new String("");
-    static String option4 = new String("");
-    static String option5 = new String("");
-    static String option6 = new String("");
-    JButton button = new JButton("Convert");
-    JButton button2 = new JButton("Upload txt file");
+    static JButton convertButton = new JButton("Convert");
+    static JButton uploadFileButton = new JButton("Upload txt file");
 
     
     Font font = new Font("Times new Roman", Font.BOLD, 14);
 
-    JTextField inputOpt1 = new JTextField("",10);   
-    JTextField inputOpt2 = new JTextField("",10);  
-    
-    JLabel opt3 = new JLabel("max length?");
-    JTextField inputOpt3 = new JTextField("",10);  
-    
-    JLabel fi1 = null;
+    JTextField inputNumResidues = new JTextField("",10);   
+    JLabel infileName = null;
 
-    public static ArrayList<dist> topList;
-	Calculator() {
-        super("Angle and Distance Calculator (Average)");
+    String[] curArgs = new String[3];
+
+	Converter() {
+        super(".txt Data Points to .pdb Format Converter");
         setDesign();
         setSize(500, 500);
         setResizable(true);
@@ -63,7 +68,7 @@ public class Converter() {
 
         row[14].setLayout(f1);
 
-        button2.addActionListener(new ActionListener() {
+        uploadFileButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
 
@@ -78,117 +83,119 @@ public class Converter() {
             			curArgs[0]= jfc.getSelectedFile().getAbsolutePath();
             			txtName = jfc.getSelectedFile().getName();
             			
-            			fi1 = new JLabel(txtName);
+            			infileName = new JLabel(txtName);
             			
-            			row[0].add(fi1);
+            			row[0].add(infileName);
             			add(row[0]);
             			setVisible(true);
             		}     
             }
         }); 
 
-        inputOpt1.addKeyListener(new KeyAdapter() {
-            public void keyReleased(KeyEvent event) {
-                String content = inputOpt1.getText();
-                option1 = content;
-            }           
-        });
-
-        inputOpt2.addKeyListener(new KeyAdapter() {
-            public void keyReleased(KeyEvent event) {
-                String content = inputOpt2.getText();
-                option2 = content;
-            }           
-        });
-        
-        inputOpt3.addKeyListener(new KeyAdapter() {
-            public void keyReleased(KeyEvent event) {
-                String content = inputOpt3.getText();
-                maxLength = Integer.parseInt(content);
-            }           
-        });
-
-        button.addActionListener(new ActionListener() {
+        convertButton.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent event) {
             	
                 try{
                 	PrintWriter pw = new PrintWriter(new FileWriter("Txt_to_pdb_" + txtName + ".txt", false));
                 	pw.println("This is the .pdb file to be printed to");
+                    try {
+                        String numRes = inputNumResidues.getText();
+                        numResidues = Integer.parseInt(numRes);
+                        printPointsToFile(txtName, pw);
+                    }
+                    catch(NumberFormatException e) {
+                        System.exit(-1);
+                    }
 
-                	try{
-                		printToFile(txtName, pw);
-                	}
-
+                	printPointsToFile(txtName, pw);
                     System.out.println("write done");
-
-                	}finally{
-                		pw.close();
-                        graph2.setVisible(true);
-                	}
-                		
-                } catch (IOException e){
-                	                	
+                } 
+                catch (IOException e) {                	
                     System.exit(-1);
                 }
+                finally{
+                        pw.close();
+                }   
             }
         });   
 
-        button2.setForeground(Color.BLACK);
-        button2.setBackground(Color.WHITE);
-        button3.setForeground(Color.BLACK);
-        button3.setBackground(Color.WHITE);
+        uploadFileButton.setForeground(Color.BLACK);
+        uploadFileButton.setBackground(Color.WHITE);
+        convertButton.setForeground(Color.BLACK);
+        convertButton.setBackground(Color.WHITE);
 
-        inputOpt1.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-        inputOpt1.setPreferredSize(regularDimension);
-        inputOpt2.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-        inputOpt2.setPreferredSize(regularDimension);
-        inputOpt3.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-        inputOpt3.setPreferredSize(regularDimension);
-        message.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-        message.setPreferredSize(regularDimension);
-
-        button.setForeground(Color.BLACK);
-        button.setBackground(Color.WHITE);
         Border line = new LineBorder(Color.BLACK);
         Border margin = new EmptyBorder(5, 15, 5, 15);
         Border compound = new CompoundBorder(line, margin);
-        button2.setBorder(compound);
-        button3.setBorder(compound);
-        button.setBorder(compound);
+        uploadFileButton.setBorder(compound);
+        convertButton.setBorder(compound);
         
-        row[2].add(button2);
-        row[3].add(button3);
+        row[2].add(uploadFileButton);
+  
+        row[12].add(convertButton);
 
-        row[6].add(opt1);
-        row[7].add(inputOpt1);
-        row[8].add(opt2);
-        row[9].add(inputOpt2);
-        row[10].add(opt3);
-        row[11].add(inputOpt3);
-        row[12].add(button);
-
-        row[13].add(message);
         for(int i=2;i<14;i++){
             add(row[i]);
         } 
         setVisible(true);
     }
 
-    public static void printToFile(String txtName, Printwriter pw) {
+    public static void printPointsToFile(String txtName, Printwriter pw) {
     	try {
-    		File pdbfile = new File (pdb1);
-            Scanner s = new Scanner (pdbfile);
+    		File txtFile = new File (txtName);
+            Scanner s = new Scanner (txtFile);
 
             int atomNum = 1;
 
             while(s.hasNextLine()) {
             	String line = s.nextLine();
-            	String residue1 = line.substring(0, s.firstIndexOf());
-            	line = line.substring(s.firstIndexOf()+1);
-            	String residue2 = line.substring(0, s.firstIndexOf());
-            	pw.println("ATOM\t" + atomNum + "\t" + "H\tASP\t1\t" + residue1 + "\t" + residue2);
+            	String residue1 = line.substring(0, line.indexOf(" "));
+            	line = line.substring(line.indexOf(" ")+1);
+            	String residue2 = line.substring(0, line.indexOf(" "));
+                try {
+                    int res1 = Integer.parseInt(residue1);
+                    int res2 = Integer.parseInt(residue2);
+                	pw.println("HETATM\t" + atomNum + "\t" + "H\tOAA A\t1\t" + (res1/3) + "\t" + (res2/3) + "\n");
+                    atomNum++;
+                }
+                catch (NumberFormatException e) {
+                    System.err.println("Could not print because of unexpected .txt format");
+                    System.exit(-1);
+                }
             }
+
+            printAxesToFile(pw, atomNum);
     	}
+        catch (IOException e){
+            System.err.println("Could not write to file");
+            System.exit(-1);
+        }
+    }
+
+    public static void printAxesToFile(Printwriter pw, int atomNum) {
+        try{
+            int x = 0;
+            while(x < numResidues/3) {
+                pw.println("HETATM\t" + atomNum + "\t" + "H\tOAA A\t1\t" + (x/3) + "\t0\t0\t1\t18\tH");
+                x+=1;
+            }
+
+            int y = 0;
+            while(y < numResidues/3) {
+                pw.println("HETATM\t" + atomNum + "\t" + "H\tOAA A\t1\t" + "0\t" +  (y/3) + "\t0\t1\t18\tH");
+                x+=1;
+            }
+
+            /*int z = 0;
+            while(z < ) {
+                pw.println("HETATM\t" + atomNum + "\t" + "H\tOAA A\t1\t" + x/3 + "\t0\t0\t1\t18\tH");
+                x+=1;
+            }*/
+        }
+        catch (IOException e){
+            System.err.println("Could not write axes to file");
+            System.exit(-1);
+        }
     }
 }
